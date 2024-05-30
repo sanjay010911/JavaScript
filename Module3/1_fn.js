7646fefa0a6fe170c6200926313b0e2fee97c3cc
sanjay010911 <sanjaybenoy123@gmail.com>
2024-05-30 19:38:13 +0530

    jn

--- Module3/1_fn.js
+++ Module3/1_fn.js
@@ -1,9 +1,14 @@
+
+sum(5,8)
+
 function sum(a,b){
     console.log(a+b)
 }
 
 sum(5,1)
 
+
+// console.log(add(4,5))  ->this is not possible because add() is an expression fn
 var add=function(a,b){
     return a+b
 }

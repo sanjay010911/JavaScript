ef5aae9c6b6b0cd40b59f8fb42190a0e1ff1be6a
sanjay010911 <sanjaybenoy123@gmail.com>
2024-05-31 19:39:34 +0530

    fghfgh

---
+++ Module4/1_let_const_var.js
@@ -0,0 +1,25 @@
+var c=5;
+console.log(c);
+
+{
+    var b=67;
+}
+
+console.log(b) //only var variables an be accessed outside a block
+
+let d=57;
+var c=56;
+// let d=33;  ->error as let does not allows redeclaration
+
+console.log(c)
+
+const f='ddf'
+
+// f="sdsdf"  ->error as const does not allow redeclaration or reassignment
+// const f="sdsd"
+
+console.log(f)
+
+
+
+
---
+++ Module4/2_arrow_fn.js
@@ -0,0 +1,5 @@
+let add=(a,b)=>{
+    console.log(a+b)
+}
+
+add(4,5)

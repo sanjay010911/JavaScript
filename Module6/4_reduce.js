d20851661367a1fc009fca512c3fa5a28946dd48
sanjay010911 <140841787+sanjay010911@users.noreply.github.com>
2024-06-04 07:51:11 +0530

    Delete Module6/4_reduce.js

--- Module6/4_reduce.js
+++
@@ -1,8 +0,0 @@
-const transactions=[1000,3000,4000,2000,-898,3800,-4500]
-
-const res=transactions.reduce(function(acc,val){
-    let updatedsum=acc+val
-    return updatedsum
-})
-
-console.log(res)

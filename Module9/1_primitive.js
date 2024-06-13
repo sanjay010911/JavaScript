38bcfcc4f263e9eb9b800cbfebb9974b047a312d
sanjay010911 <140841787+sanjay010911@users.noreply.github.com>
2024-06-13 22:19:55 +0530

    Create 1_primitive.js

---
+++ Module9/1_primitive.js
@@ -0,0 +1,20 @@
+let first='Adam'
+
+let second=first
+
+first="steve"
+
+console.log(first)
+console.log(second)
+
+
+let person={
+    name:"adam",
+    age:23
+}
+
+second=person
+person.name="steve"
+
+console.log(person.name)
+console.log(second)
